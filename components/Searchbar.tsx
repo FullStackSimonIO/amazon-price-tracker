6c199efd547f2a3cbf698ae9d2633b21569e53c9
FullStackSimonIO <fullstacksimon@gmail.com>
2023-12-29 18:30:11 +0100

    added searchbar updates

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,31 @@
 "use client";
 
+import { FormEvent, useState } from "react";
+
+const isValidAmazonProductUrl = (url: string) => {
+  try {
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch (error) {}
+  return false;
+};
+
 const Searchbar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductUrl(searchPrompt);
+  };
 
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
