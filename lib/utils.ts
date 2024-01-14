b3492ae557fe2d5ebeeec3580ea56c38a85043e8
FullStackSimonIO <fullstacksimon@gmail.com>
2024-01-14 20:41:23 +0100

    utils fixes

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,15 @@
 import { PriceHistoryItem, Product } from "@/types";
-import { Notification } from "@/lib/nodemailer";
-import { Product } from "@/types";
-import { THRESHOLD_PERCENTAGE } from "@/lib/nodemailer";
 
-// Extracts and returns price from list of possible items
+const Notification = {
+  WELCOME: "WELCOME",
+  CHANGE_OF_STOCK: "CHANGE_OF_STOCK",
+  LOWEST_PRICE: "LOWEST_PRICE",
+  THRESHOLD_MET: "THRESHOLD_MET",
+};
+
+const THRESHOLD_PERCENTAGE = 40;
+
+// Extracts and returns the price from a list of possible elements.
 export function extractPrice(...elements: any) {
   for (const element of elements) {
     const priceText = element.text().trim();
@@ -14,23 +20,47 @@ export function extractPrice(...elements: any) {
       let firstPrice;
 
       if (cleanPrice) {
-        firstPrice = cleanPrice.match(/\d+\.\{2}/)?.[0];
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
       }
 
       return firstPrice || cleanPrice;
     }
   }
+
   return "";
 }
 
-// Extracts and returns currency symbol from an element
+// Extracts and returns the currency symbol from an element.
 export function extractCurrency(element: any) {
   const currencyText = element.text().trim().slice(0, 1);
   return currencyText ? currencyText : "";
 }
 
-// Calculates Highest Price of Database
-/*export function getHighestPrice(priceList: PriceHistoryItem[]) {
+// Extracts description from two possible elements from amazon
+export function extractDescription($: any) {
+  // these are possible elements holding description of the product
+  const selectors = [
+    ".a-unordered-list .a-list-item",
+    ".a-expander-content p",
+    // Add more selectors here if needed
+  ];
+
+  for (const selector of selectors) {
+    const elements = $(selector);
+    if (elements.length > 0) {
+      const textContent = elements
+        .map((_: any, element: any) => $(element).text().trim())
+        .get()
+        .join("\n");
+      return textContent;
+    }
+  }
+
+  // If no matching elements were found, return an empty string
+  return "";
+}
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
   let highestPrice = priceList[0];
 
   for (let i = 0; i < priceList.length; i++) {
@@ -38,10 +68,10 @@ export function extractCurrency(element: any) {
       highestPrice = priceList[i];
     }
   }
+
   return highestPrice.price;
 }
 
-// Calculates the lowest Price of Database
 export function getLowestPrice(priceList: PriceHistoryItem[]) {
   let lowestPrice = priceList[0];
 
@@ -50,15 +80,35 @@ export function getLowestPrice(priceList: PriceHistoryItem[]) {
       lowestPrice = priceList[i];
     }
   }
+
+  return lowestPrice.price;
 }
 
-// Calculates average Price over time of Database
 export function getAveragePrice(priceList: PriceHistoryItem[]) {
   const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
   const averagePrice = sumOfPrices / priceList.length || 0;
 
   return averagePrice;
-}*/
+}
+
+export const getEmailNotifType = (
+  scrapedProduct: Product,
+  currentProduct: Product
+) => {
+  const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+
+  if (scrapedProduct.currentPrice < lowestPrice) {
+    return Notification.LOWEST_PRICE as keyof typeof Notification;
+  }
+  if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+    return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+  }
+  if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+    return Notification.THRESHOLD_MET as keyof typeof Notification;
+  }
+
+  return null;
+};
 
 export const formatNumber = (num: number = 0) => {
   return num.toLocaleString(undefined, {
