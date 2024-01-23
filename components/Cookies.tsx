1bbd931c39ef9b11baf31535f0ac5043229082e6
FullStackSimonIO <fullstacksimon@gmail.com>
2024-01-23 22:30:39 +0100

    ui fixes

--- components/Cookies.tsx
+++ components/Cookies.tsx
@@ -5,7 +5,7 @@ const Cookies = () => {
   return (
     <div>
       <CookieConsent>
-        This Website is using cookies per personalized experiencees.
+        This Website is using cookies per personalized experiencees
       </CookieConsent>
     </div>
   );
