9a9703b2ca1edbda997b1a87540b7582ed3179d9
FullStackSimonIO <fullstacksimon@gmail.com>
2024-01-24 22:08:15 +0100

    more animation

--- components/Cookies.tsx
+++ components/Cookies.tsx
@@ -5,7 +5,7 @@ const Cookies = () => {
   return (
     <div>
       <CookieConsent>
-        This Website is using cookies per personalized experience
+        This website is using cookies per personalized experience
       </CookieConsent>
     </div>
   );
